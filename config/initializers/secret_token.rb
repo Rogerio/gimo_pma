0e983d2995086745793cdc675fc6be5deee10c54
Henrique H. da Silva <henrikesilva1@gmail.com>
2011-03-26 21:09:48 -0300

    Iniciando Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gimo::Application.config.secret_token = '1a2b16449b3b16a6c1cf652ee590d9b44c8de0ea1a9b8887e67bec975751a219010100559e72842e9d43261eaa47806c65eae08fcb56b4058aa9103fc1963ce3'
